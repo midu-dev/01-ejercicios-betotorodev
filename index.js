49df59b66642b176eb28b0c27819136c528625a9
Beto Toro <beto.toro@zemoga.com>
2023-07-31 05:19:25 -0500

    feat: finish first ejercise

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-// Ejercicio 2
-async function writeFile (filePath, data, callback) {
+const fs = require("node:fs/promises")
+const { join } = require("node:path")
 
+// Ejercicio 2
+async function writeFile(filePath, data, callback) {
+  const projectFolder = join(__dirname, filePath)
+
+  try {
+    await fs.mkdir(projectFolder, { recursive: true })
+  } catch(err) {
+    console.log(`Error al crear la carpeta, aquí más información => ${err}`)
+  }
+
+  try {
+    await fs.writeFile(projectFolder, data)
+  } catch(err) {
+    console.log(`Error al crear el archivo, aquí más información => ${err}`);
+  }
+  callback()
 }
 
 // Ejercicio 3
-async function readFileAndCount (word, callback) {
-
-}
+async function readFileAndCount(word, callback) {}
 
 module.exports = {
   writeFile,
